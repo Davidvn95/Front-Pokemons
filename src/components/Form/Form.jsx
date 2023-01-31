bb308c5261a15afcf9a206c62b7d2ff6c8896717
Davidvn95 <davidvergaraok@gmail.com>
2023-01-31 15:02:48 -0500

    Uploading Front

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    cleanForm,
+    createPokemon,
+    getAllPokemons,
+    getAllTypes,
+} from "../../redux/actions/actions";
+import Card from "../Card/Card";
+import styles from "./Form.module.css";
+
+const Form = () => {
+    const created = useSelector((state) => state.created);
+    const types = useSelector((state) => state.types);
+    const dispatch = useDispatch();
+
+    const [newPokemon, setnewPokemon] = useState({
+        name: "",
+        life: "",
+        attack: "",
+        defense: "",
+        speed: "",
+        weight: "",
+        height: "",
+        image: "",
+        type: [],
+    });
+
+    const cleaningForm = () => {
+        dispatch(cleanForm());
+    };
+
+    const handleForm = (event) => {
+        const property = event.target.name;
+        const { value } = event.target;
+        if (property !== "type" && property !== "type2") {
+            setnewPokemon({
+                ...newPokemon,
+                [property]: value,
+            });
+        } else {
+            if (property === "type2") {
+                setnewPokemon({
+                    ...newPokemon,
+                    [property.slice(0, -1)]: newPokemon.type
+                        ? [newPokemon.type[0], value]
+                        : [value],
+                });
+            } else {
+                setnewPokemon({
+                    ...newPokemon,
+                    [property]: [value],
+                });
+            }
+        }
+    };
+
+    const submitFunction = (event) => {
+        event.preventDefault();
+        if (
+            newPokemon.name?.length > 0 &&
+            newPokemon.name?.length <= 10 &&
+            newPokemon.type?.length
+        ) {
+            const body = {};
+            for (const prop in newPokemon) {
+                if (newPokemon[prop] !== "") body[prop] = newPokemon[prop];
+            }
+            dispatch(createPokemon(body));
+            dispatch(getAllPokemons());
+            dispatch(getAllTypes());
+        } else if (newPokemon.name?.length > 10) {
+            alert("length name's must be less 10")
+        }else alert("Name and Type are required");
+    };
+
+    return created.id ? (
+        <div className={styles.cards}>
+            <Card
+                id={created.id}
+                name={created.name}
+                image={created.image}
+                type={created.type ? created.type : created.types}
+            />
+            <button onClick={cleaningForm}>New</button>
+        </div>
+    ) : (
+        <div className={styles.container}>
+            <h2>Creation form</h2>
+            <div className={styles.form_Img}>
+                <form>
+                    <div className={styles.info}>
+                        <label htmlFor="name">Name*: </label>
+                        <div className={styles.errorName}>
+                            <input
+                                type="text"
+                                name="name"
+                                onChange={handleForm}
+                                value={newPokemon.name}
+                                placeholder="Enter a Name"
+                                autoComplete="off"
+                            />
+                            {newPokemon.name?.length > 10 && (
+                                <span>*Enter 10 characters or less*</span>
+                            )}
+                        </div>
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="life">Life: </label>
+                        <input
+                            type="number"
+                            name="life"
+                            onChange={handleForm}
+                            value={newPokemon.life}
+                            placeholder="Deafult 1"
+                        />
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="attack">Attack: </label>
+                        <input
+                            type="number"
+                            name="attack"
+                            onChange={handleForm}
+                            value={newPokemon.attack}
+                            placeholder="Default 1"
+                        />
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="defense">Defense: </label>
+                        <input
+                            type="number"
+                            name="defense"
+                            onChange={handleForm}
+                            value={newPokemon.defense}
+                            placeholder="Default 1"
+                        />
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="speed">Speed: </label>
+                        <input
+                            type="number"
+                            name="speed"
+                            onChange={handleForm}
+                            value={newPokemon.speed}
+                            placeholder="Default 1"
+                        />
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="weight">Weight: </label>
+                        <input
+                            type="number"
+                            name="weight"
+                            onChange={handleForm}
+                            value={newPokemon.weight}
+                            placeholder="Default 1"
+                        />
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="height">Height: </label>
+                        <input
+                            type="number"
+                            name="height"
+                            onChange={handleForm}
+                            value={newPokemon.height}
+                            placeholder="Default 1"
+                        />
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="image">Image: </label>
+                        <input
+                            type="text"
+                            name="image"
+                            onChange={handleForm}
+                            value={newPokemon.image}
+                            placeholder="Enter a URL"
+                        />
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="type">Type*: </label>
+                        <select name="type" onChange={handleForm}>
+                            <option>Select a type</option>
+                            {types.map((typ, index) => (
+                                <option key={index}>{typ.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className={styles.info}>
+                        <label htmlFor="type2">Type2 (optinal): </label>
+                        <select name="type2" onChange={handleForm}>
+                            <option value="null">Select a type</option>
+                            {types.map((typ, index) => (
+                                <option key={index}>{typ.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <button onClick={submitFunction}>Submit</button>
+                </form>
+                <img
+                    src={
+                        newPokemon.image || "https://i.ibb.co/zPfjMYc/Pokemon-desconocido.jpg"
+                    }
+                    alt="Created"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Form;
